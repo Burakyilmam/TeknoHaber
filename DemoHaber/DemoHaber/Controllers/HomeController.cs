8c624a1faaf157bb0371ceca06065187bc9f473a
Burak Yılmam <61635780+Burakyilmam@users.noreply.github.com>
2021-06-15 15:22:50 +0300

    TeknoHaber

--- DemoHaber/DemoHaber/Controllers/HomeController.cs
+++ DemoHaber/DemoHaber/Controllers/HomeController.cs
@@ -11,14 +11,14 @@ namespace DemoHaber.Controllers
     public class HomeController : Controller
     {
         Context c = new Context();
-        public ActionResult HaberListele(String p)
+        public ActionResult HaberListele(String p , int sayfa = 1)
         {
             var degerler = from x in c.Habers select x;
             if (!string.IsNullOrEmpty(p))
             {
                 degerler = degerler.Where(y => y.HaberBaslik.Contains(p));
             }
-            var listele = degerler.OrderByDescending(x => x.HaberEklenmeTarihi).Where(x => x.HaberEklenmeTarihi <= DateTime.Today).ToList();
+            var listele = degerler.OrderByDescending(x => x.HaberEklenmeTarihi).Where(x => x.HaberEklenmeTarihi <= DateTime.Today).ToList().ToPagedList(sayfa,3);
             return View(listele);
         }
         public ActionResult HaberSayfa(int id)
@@ -26,29 +26,29 @@ namespace DemoHaber.Controllers
             var degerler = c.Habers.Where(x => x.HaberId == id).ToList();
             return View(degerler);
         }
-        public ActionResult KategoriGetir(int id)
+        public ActionResult KategoriGetir(int id , int sayfa = 1)
         {
-            var kategoris = c.Habers.OrderByDescending(x => x.HaberEklenmeTarihi).Where(x => x.Kategori.KategoriId == id).ToList();
+            var kategoris = c.Habers.OrderByDescending(x => x.HaberEklenmeTarihi).Where(x => x.Kategori.KategoriId == id).ToList().ToPagedList(sayfa,5);
             return View(kategoris);
         }
-        public ActionResult YeniEklenen(String p)
+        public ActionResult YeniEklenen(String p , int sayfa = 1)
         {
             var degerler = from x in c.Habers select x;
             if (!string.IsNullOrEmpty(p))
             {
                 degerler = degerler.Where(y => y.HaberBaslik.Contains(p));
             }
-            var listele = degerler.OrderByDescending(x => x.HaberEklenmeTarihi).Where(x => x.HaberEklenmeTarihi <= DateTime.Today).ToList();
+            var listele = degerler.OrderByDescending(x => x.HaberEklenmeTarihi).Where(x => x.HaberEklenmeTarihi <= DateTime.Today).ToList().ToPagedList(sayfa,5);
             return View(listele);
         }
-        public ActionResult Yorumlanan(String p)
+        public ActionResult Yorumlanan(String p , int sayfa = 1)
         {
             var degerler = from x in c.Habers select x;
             if (!string.IsNullOrEmpty(p))
             {
                 degerler = degerler.Where(y => y.HaberBaslik.Contains(p));
             }
-            var listele = degerler.OrderByDescending(x => x.Yorums.Count()).ToList();
+            var listele = degerler.OrderByDescending(x => x.Yorums.Count()).ToList().ToPagedList(sayfa,5);
             return View(listele);
 
         }
